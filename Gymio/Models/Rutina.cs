81d62e45cde58d32b8927f361d8146ce9c102ff0
Aldo Aviles <aldoavile11@gmail.com>
2026-04-24 14:16:16 -0600

    clase de rutina

--- Gymio/Models/Rutina.cs
+++ Gymio/Models/Rutina.cs
@@ -1,10 +1,11 @@
-﻿using MongoDB.Bson;
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Gymio.Models
 {
 
-    //esto falta mejorarlo esta para mientras
+  
     public class Rutina
     {
         [BsonId]
@@ -12,19 +13,44 @@ namespace Gymio.Models
         public string? Id { get; set; }
 
         public int ClienteId { get; set; } 
+        public int EntrenadorId { get; set; }
 
-        public string NombreRutina { get; set; } = string.Empty; // Ej: "hipertrofia - Pecho"
+        public string Titulo { get; set; }
+        public string Objetivo { get; set; }// "fuerza hiper, "
 
-        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
 
-        public List<Ejercicio> Ejercicios { get; set; } = new();
+        public string IndicacionesGenrales {  get; set; }
+        public bool Activa { get; set; } 
+
+        public List<DiaEntrenamiento> Dias { get; set; }
+
+
     }
 
-    public class Ejercicio
+    public class DiaEntrenamiento
     {
-        public string Nombre { get; set; } = string.Empty;
+        public int OrdenDia { get; set; }
+        public string NombreDia { get; set; }
+        public string ?FocoMuscular { get; set; } // "pecho", "pierna", "fullbody"
+        //los ejercicios conectarlo
+        public List<EjercicioRutina> Ejercicios { get; set; }
+
+    }
+
+    public class EjercicioRutina
+    {
+
+        public string NombreEjercicio { get; set; }
         public int Series { get; set; }
-        public string Repeticiones { get; set; } = string.Empty;
-        public string? Notas { get; set; }
+        public string Repeticiones { get; set; }
+        public int DescansoSegundos { get; set; }
+
+
+        public string ?RIR { get; set; } // "2 RIR", "3 RIR", "Al fallo"
+        public string ?Tempo { get; set; } // "2-0-1", "3-1-1", "4-0-2"
+        public string ?NotasCoach { get; set; } // "Mantener la espalda recta", "Controlar el movimiento en la fase excéntrica"
+        public string ?UrlVideoReferencia { get; set; } // "https://www.youtube.com/watch?v=EjemploVideo"
     }
 }
