2b802de8b7012ebccc0785cf47d0321535ba25ab
Aldo Aviles <aldoavile11@gmail.com>
2026-04-24 22:49:49 -0600

    lista de clientes

--- Gymio/Services/ClienteService.cs
+++ Gymio/Services/ClienteService.cs
@@ -1,5 +1,6 @@
 ﻿using Gymio.Data;
 using Gymio.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gymio.Services
 {
@@ -13,13 +14,13 @@ namespace Gymio.Services
             _context = context;
         }
 
-        
+
         public async Task<bool> RegistrarClienteAsync(Cliente nuevoCliente)
         {
             if (string.IsNullOrWhiteSpace(nuevoCliente.Nombre) || string.IsNullOrWhiteSpace(nuevoCliente.Apellido))
                 return false;
 
-          
+
             string iniciales = $"{nuevoCliente.Nombre.Substring(0, 1)}{nuevoCliente.Apellido.Substring(0, 1)}".ToUpper();
             string timestamp = DateTime.Now.ToString("yyMM");
             int randomNum = new Random().Next(1000, 9999);
@@ -34,6 +35,24 @@ namespace Gymio.Services
             await _context.SaveChangesAsync(); // Usamos la versión Async
 
             return true;
+        }
+
+        public async Task<List<Cliente>> ObtenerClientesAsync(string terminoBusqueda = "")
+        {
+
+            var consulta = _context.Clientes.AsQueryable();
+
+        
+            if (!string.IsNullOrWhiteSpace(terminoBusqueda))
+            {
+                consulta = consulta.Where(c =>
+                    c.Nombre.Contains(terminoBusqueda) ||
+                    c.Apellido.Contains(terminoBusqueda) ||
+                    c.CodigoQR.Contains(terminoBusqueda));
+            }
+
+          
+            return await consulta.OrderByDescending(c => c.FechaRegistro).ToListAsync();
         }
     }
 }
